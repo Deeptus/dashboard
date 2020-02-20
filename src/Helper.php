d8e2c4674199bd726916a67b04f5897269f3848d
Alfonzo Diez <alfonzodiez@gmail.com>
2020-02-20 06:22:58 -0400

    Commit Rapido

--- src/Helper.php
+++ src/Helper.php
@@ -114,4 +114,44 @@ if (!function_exists('__slug')) {
     }
 }
 
+if (!function_exists('__dolar')) {
+    function __dolar()
+    {
+        /*
+        // Esto estaba en la vista producto
+        $data_in = "http://ws.geeklab.com.ar/dolar/get-dolar-json.php";
+        $data_json = file_get_contents($data_in);
+        if(strlen($data_json)>0) {
+          $data_out = json_decode($data_json,true);
+        }
+        $usd=$data_out['libre'];
+        */
+        $client = new \Goutte\Client(\Symfony\Component\HttpClient\HttpClient::create(['timeout' => 60]));;
+        if (@fsockopen('www.bna.com.ar', 80)) {
+            // Seteo para que no se caiga por tiempo
+            /*
+            $guzzleClient = new \GuzzleHttp\Client(array(
+                'timeout' => 90,
+                'verify' => false,
+            ));
+            $client->setClient($guzzleClient);
+            */
+
+            $dolarPrice = Illuminate\Support\Facades\Cache::remember('dolar', 60, function () use ($client) {
+                $crawler = $client->request('GET', 'http://www.bna.com.ar/Personas');
+                $table   = 'table cotizacion';
+
+                $dolar   = $crawler->filter("[class='$table']")->filter('tr')->filter('td')->siblings();
+                $dolar   = $dolar->nextAll();
+                return $dolar->text();
+            });
+
+            return $dolarPrice;
+        } else {
+            return 63;
+        }
+
+    }
+}
+
 ?>
