9b1d830f7439de9b1753e2e857d57d9a2732958a
Alfonzo Diez <alfonzodiez@gmail.com>
2020-11-30 18:07:05 -0400

    update GenerateCrudTables

--- src/migrations/2020_11_23_000001_generate_crud_tables.php
+++ src/migrations/2020_11_23_000001_generate_crud_tables.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Migrations\Migration;
 use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\App;
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\DBAL\Types\Type;
 
 class GenerateCrudTables extends Migration
 {
@@ -20,35 +22,15 @@ class GenerateCrudTables extends Migration
         $files = File::allFiles($dirPath);
         foreach ($files as $fileKey => $file) {
             $content = json_decode(file_get_contents($file->getPathname()));
-            Schema::create($content->table->tablename, function (Blueprint $table) use ($content) {
-                $table->bigIncrements('id');
-                $table->uuid('uuid');
-                foreach ($content->inputs as $inputKey => $input) {
-                    if (!Schema::hasColumn($content->table->tablename, $input->columnname)) {
-                        if($input->type == 'text') {
-                            $col = $table->string($input->columnname);
-                        }
-                        if($input->type == 'textarea') {
-                            $col = $table->longText($input->columnname);
-                        }
-                        if($input->type == 'number') {
-                            $col = $table->double($input->columnname);
-                        }
-                        if($input->type == 'true_or_false') {
-                            $col = $table->boolean($input->columnname);
-                        }
-                        if($input->type == 'select') {
-                            $col = $table->unsignedBigInteger($input->columnname);
-                        }
-                        if($input->nullable == 1) {
-                            $col->nullable();
-                        }
-
-                    }
-                }
-                $table->timestamps();
-                $table->softDeletes();
-            });
+            if (Schema::hasTable($content->table->tablename)) {
+                Schema::table($content->table->tablename, function (Blueprint $table) use ($content) {
+                    $this->table($table, $content);
+                });
+            } else {
+                Schema::create($content->table->tablename, function (Blueprint $table) use ($content) {
+                    $this->table($table, $content);
+                });
+            }
         }
     }
 
@@ -65,4 +47,50 @@ class GenerateCrudTables extends Migration
             }
         });
     }
+    public function table($table, $content)
+    {
+        // added support double to change
+        if (!Type::hasType('double')) {
+            Type::addType('double', FloatType::class);
+        }
+
+        if (!Schema::hasColumn($content->table->tablename, 'id')) {
+            $table->bigIncrements('id');
+        }
+        if (!Schema::hasColumn($content->table->tablename, 'uuid')) {
+            $table->uuid('uuid');
+        }
+        foreach ($content->inputs as $inputKey => $input) {
+            $change = false;
+            if (Schema::hasColumn($content->table->tablename, $input->columnname)) {
+                $change = true;
+            }    
+            if($input->type == 'text') {
+                $col = $table->string($input->columnname);
+            }
+            if($input->type == 'textarea') {
+                $col = $table->longText($input->columnname);
+            }
+            if($input->type == 'number') {
+                $col = $table->double($input->columnname);
+                $change = false;
+            }
+            if($input->type == 'true_or_false') {
+                $col = $table->boolean($input->columnname);
+            }
+            if($input->type == 'select') {
+                $col = $table->unsignedBigInteger($input->columnname);
+            }
+            if($input->nullable == 1) {
+                $col->nullable();
+            }
+            $col->change();
+        }
+        if (!Schema::hasColumn($content->table->tablename, 'created_at')) {
+            $table->timestamps();
+        }
+        if (!Schema::hasColumn($content->table->tablename, 'deleted_at')) {
+            $table->softDeletes();
+        }
+}
 }
